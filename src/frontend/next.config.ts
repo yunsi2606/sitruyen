1de3795182f6fe58f4bcffd7cf6dc9096b18d4cd
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2026-02-13 15:31:39 +0700

    ops: add docker deployment for frontend and cloudflare tunnel

--- src/frontend/next.config.ts
+++ src/frontend/next.config.ts
@@ -2,7 +2,32 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   reactCompiler: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '1337',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+      },
+      {
+        protocol: 'http',
+        hostname: 'sitruyen-strapi', // Internal docker network
+        port: '1337',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
